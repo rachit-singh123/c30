f7d1da8140d5ddc814759fd9b815f9880aacd1ef
rachit-123 <jashitsingh@gmail.com>
2021-04-29 17:59:36 +0530

    changes have been made

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingShot;
 
@@ -23,12 +23,12 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+    pig1 = new pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig3 = new pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -74,4 +74,9 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+function keyPressed(){
+    if(keyCode=== 32){
+        slingshot.attach(bird.body)
+    }
 }
